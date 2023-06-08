b189dbbff7635ccc9147b3c69ba4b6968afedd40
OliviaEmefa <oliviaemefa2004@gmail.com>
2023-06-07 22:24:47 -0700

    first commit

---
+++ login.js
@@ -0,0 +1,6 @@
+//Create a login form which takes a username , password and submit button.
+//Check if the username is an alphabet and more than 2 characters
+//on each instance provide an error message as feedback to the user
+//check if the password is atleast 8 characters
+//Make sure submit button doesn't refresh the page
+//All evaluations must be done if an only if submit button is cllicked
